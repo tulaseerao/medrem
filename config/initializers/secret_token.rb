aa0eb66293685c3eab4f9da90a07fee45ed39411
tulaseerao <tulasi.chintha@gmail.com>
2013-12-30 20:45:23 -0500

    Added MedRem Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Medrem::Application.config.secret_key_base = '885095471c4e007ebbbe89d83e78b52a911ab324cbc04353e75cb6a13539e60db6a759a8f378f6c306eeb7a6497d9bdf6dbc801ec716267fe4e3b992c976f276'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Medrem::Application.config.session_store :cookie_store, key: '_medrem_session'
