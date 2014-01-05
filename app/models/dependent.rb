e86e48dc307bcd29bd5034ee4a2448baf9f6be44
OpenShift guest <mikeodell77@gmail.com>
2014-01-05 12:05:21 -0500

    Adding dependents

---
+++ app/models/dependent.rb
@@ -0,0 +1,2 @@
+class Dependent < ActiveRecord::Base
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140102045905) do
+ActiveRecord::Schema.define(version: 20140104220819) do
+
+  create_table "dependents", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.date     "date_of_birth"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
